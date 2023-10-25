2f60a89b12e70e9eabbb4224280676e001397ab3
David Madsen <dnm070902@gmail.com>
2023-10-25 19:49:19 +0200

    Added 'forgot password' text.

--- app/src/main/java/com/example/mediaapp/MainActivity.kt
+++ app/src/main/java/com/example/mediaapp/MainActivity.kt
@@ -3,22 +3,13 @@ package com.example.mediaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -29,7 +20,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -41,9 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.KeyboardType
@@ -51,11 +39,10 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mediaapp.ui.theme.MediaAppTheme
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -94,7 +81,7 @@ fun LoginPageLayout() {
             color = Color.White,
             modifier = Modifier
                 .padding(start = 29.dp, top = 45.dp))
-        Text(stringResource(R.string.please),
+        Text(stringResource(R.string.login_please),
             fontSize = 12.sp,
             lineHeight = 28.sp,
             color = Color.White,
@@ -102,6 +89,15 @@ fun LoginPageLayout() {
                 .padding(start = 30.dp))
         TextfieldForEmail()
         TextfieldForPassword()
+        Text(stringResource(R.string.login_forgot_password),
+            modifier = Modifier
+                .padding(start = 250.dp, top = 11.dp, end = 29.dp)
+                .fillMaxWidth(),
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            color = Color.White,
+            textDecoration = TextDecoration.Underline
+        )
     }
 }
 
@@ -184,13 +180,12 @@ fun TextfieldForPassword() {
                 Icon(imageVector  = image, description)
             }
         }
-
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainPagePreview() {
+fun LoginPagePreview() {
     MediaAppTheme {
         LoginPageLayout()
     }
