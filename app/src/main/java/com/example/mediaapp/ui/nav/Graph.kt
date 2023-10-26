d14729049e4d129352895a0e239d2e888109ac71
PH4NTOM <Simonmadsen99@gmail.com>
2023-10-26 02:06:33 +0200

    Restructure of code

--- app/src/main/java/com/example/mediaapp/ui/nav/Graph.kt
+++ app/src/main/java/com/example/mediaapp/ui/nav/Graph.kt
@@ -1,16 +1,17 @@
-package com.example.mediaapp
+package com.example.mediaapp.ui.nav
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.mediaapp.Screen
 import com.example.mediaapp.screens.HomeScreen
 import com.example.mediaapp.screens.FollowingScreen
 import com.example.mediaapp.screens.WatchlistScreen
 import com.example.mediaapp.screens.SearchScreen
 
 @Composable
-fun NavBottomGraph(navController: NavHostController) {
+fun NavigationGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Home.route) {
         composable(route = Screen.Home.route) {
             HomeScreen(navController = navController)
---
+++ app/src/main/java/com/example/mediaapp/ui/nav/Navigation.kt
@@ -0,0 +1,105 @@
+package com.example.mediaapp.ui.nav
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatListBulleted
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Subscriptions
+import androidx.compose.material.icons.outlined.FormatListBulleted
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Subscriptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.mediaapp.BottomNavItem
+import com.example.mediaapp.R
+
+@Composable
+fun BottomNavBar(modifier : Modifier = Modifier, navController: NavController) {
+    val containerColor = colorResource(R.color.black_navbar)
+    val contentColor = colorResource(R.color.white_navitem)
+    val indicatorColor = colorResource(R.color.indicator_color_navbar)
+
+    val bottomNavItems = listOf(
+        BottomNavItem(
+            name = "Home",
+            route = "home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        BottomNavItem(
+            name = "Following",
+            route = "following",
+            selectedIcon = Icons.Filled.Subscriptions,
+            unselectedIcon = Icons.Outlined.Subscriptions
+        ),
+        BottomNavItem(
+            name = "Watchlist",
+            route = "watchlist",
+            selectedIcon = Icons.Filled.FormatListBulleted,
+            unselectedIcon = Icons.Outlined.FormatListBulleted
+        ),
+        BottomNavItem(
+            name = "Search",
+            route = "search",
+            selectedIcon = Icons.Filled.Search,
+            unselectedIcon = Icons.Outlined.Search
+        ),
+    )
+
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar(
+        containerColor = containerColor,
+        tonalElevation = 0.dp,
+    ) {
+        bottomNavItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = contentColor,
+                    unselectedIconColor = contentColor,
+                    indicatorColor = indicatorColor
+                ),
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navController.navigate(item.route)
+                },
+                label = {
+                    Text(text = item.name, color = contentColor)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItemIndex) {
+                            item.selectedIcon
+                        } else item.unselectedIcon,
+                        contentDescription = item.name
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNavigation() {
+    val navController = rememberNavController()
+    BottomNavBar(navController = navController)
+}
