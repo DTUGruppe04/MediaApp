507f5ce3f8df98fa00f11350a38ab2d43f0ba88e
S185439 <113907421+S185439@users.noreply.github.com>
2023-10-29 00:58:59 +0200

    Top picture added, poster for movie description added, title for movie description added, tags added

--- app/src/main/java/com/example/mediaapp/MainActivity.kt
+++ app/src/main/java/com/example/mediaapp/MainActivity.kt
@@ -5,44 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mediaapp.ui.theme.MediaAppTheme
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieDetailPage()
+            MovieDescription("long movie description", "Oppenheimer")
         }
     }
 }
@@ -50,13 +43,81 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieDetailPage() {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color(0xFF2E2E2E))) {
         item {
             //Top part
-            Box(modifier = Modifier.fillMaxWidth().height(20.dp)) {
-
-            }
+            Image(painter = painterResource(R.drawable.oppenheimer),
+                contentDescription = "Oppenheimer",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop)
         }
     }
 }
+//060404
+@Composable
+fun MovieDescription(description: String, title: String) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = Color(0xFF3F3F3F))
+        .height(250.dp)) {
+        Image(painter = painterResource(
+            id = R.drawable.oppenheimerposter),
+            contentDescription = "Oppenheimer poster",
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .padding(bottom = 50.dp))
+        Text(text = title,
+            modifier = Modifier
+                .padding(start = 130.dp)
+                .padding(top = 10.dp),
+            color = Color.White,
+            fontSize = 30.sp)
+        Row(modifier = Modifier
+            .padding(start = 130.dp)
+            .padding(top = 60.dp)
+            .height(20.dp)
+            .width(80.dp)) {
+            TagBox(shape = RoundedCornerShape(10.dp), "Drama")
+            TagBox(shape = RoundedCornerShape(10.dp), "Bibliography")
+            TagBox(shape = RoundedCornerShape(10.dp), "Drama")
+        }
+    }
+}
+
+//can be made more customizable by making the textlength decide the length of the box
+@Composable
+fun TagBox(shape : Shape, tag : String) {
+    Column(modifier = Modifier
+        .fillMaxWidth()) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .clip(shape)
+            .background(color = Color(0xFF282727))) {
+            Text(text = tag, color = Color.White, modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
