84a51f36826e2127c99589974cd913bacd631f4b
Kevin Christian Eriksson <keerik@ttu.ee>
2023-10-28 17:15:36 +0200

    Created search bar and search page

--- app/src/main/java/com/example/mediaapp/MainActivity.kt
+++ app/src/main/java/com/example/mediaapp/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.mediaapp.ui.theme.MediaAppTheme
-import com.example.mediaapp.screens.FollowingListPage
+import com.example.mediaapp.screens.SearchPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MediaAppTheme {
-                FollowingListPage()
+                SearchPage()
                 }
             }
         }
--- app/src/main/java/com/example/mediaapp/ui/SearchBar.kt
+++ app/src/main/java/com/example/mediaapp/ui/SearchBar.kt
@@ -1,17 +1,17 @@
 package com.example.mediaapp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -32,23 +33,37 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.mediaapp.screens.SearchPage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar() {
     var text by remember { mutableStateOf(TextFieldValue("")) }
+    var isFocused by remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier
-            .padding(4.dp)
-            .width(360.dp)
+            .fillMaxWidth()
+            .padding(8.dp)
             .height(56.dp)
             .background(
-                color = Color(0xFF2B2930),
-                shape = RoundedCornerShape(size = 28.dp)),
+                shape = RoundedCornerShape(size = 28.dp),
+                color = Color(0xFF2B2930)
+            ),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceEvenly
 
     ) {
+        if (isFocused) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "Back arrow",
+                tint = Color(0xFFCAC4D0),
+                modifier = Modifier
+                    .clickable {}
+                    .padding(start = 16.dp)
+            )
+        }
         TextField(
             value = text,
             onValueChange = {text = it},
@@ -62,22 +77,42 @@ fun SearchBar() {
                         color = Color(0xFFCAC4D0),
                         letterSpacing = 0.5.sp
                     ),
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                 )
             },
             colors = TextFieldDefaults.textFieldColors(
+                textColor = Color(0xFFCAC4D0),
                 containerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
             ),
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+                .onFocusChanged { focusState ->
+                    isFocused = focusState.isFocused
+                }
         )
-
-        Spacer(modifier = Modifier.width(12.dp))
-
-        Box(modifier = Modifier.padding(8.dp)){
-            Icon(
-                Icons.Filled.Search,
-                tint = Color(0xFFCAC4D0),
-                contentDescription = null)
+        if (isFocused) {
+            Box(modifier = Modifier.padding(end = 24.dp)){
+                Icon(
+                    imageVector = Icons.Filled.Clear,
+                    contentDescription = "Clear icon",
+                    tint = Color(0xFFCAC4D0),
+                    modifier = Modifier.clickable { text = TextFieldValue("") }  // clear the text when clicked
+                )
+            }
+        } else {
+            Box(modifier = Modifier.padding(end = 24.dp))
+            {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search icon",
+                    tint = Color(0xFFCAC4D0)
+                )
+            }
         }
     }
 }
@@ -85,9 +120,5 @@ fun SearchBar() {
 @Preview
 @Composable
 fun SearchBarPreview(){
-    Column {
-        SearchBar()
-
-        // Rest of your screen content...
-    }
+    SearchPage()
 }
