6b223731b36f84774680908ac275bb128930d0b4
Kevin Christian Eriksson <keerik@ttu.ee>
2023-10-29 12:41:50 +0100

    Removed search page

--- app/src/main/java/com/example/mediaapp/MainActivity.kt
+++ app/src/main/java/com/example/mediaapp/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.mediaapp.ui.theme.MediaAppTheme
-import com.example.mediaapp.screens.SearchPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MediaAppTheme {
-                SearchPage()
                 }
             }
         }
