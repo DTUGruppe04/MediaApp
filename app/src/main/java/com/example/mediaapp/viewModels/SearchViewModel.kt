80e22d36d56277aca575d41297d1eb3ebbfeeb2f
Kevin Christian Eriksson <keerik@ttu.ee>
2023-11-21 11:28:55 +0100

    Improved viewmodel

--- app/src/main/java/com/example/mediaapp/viewModels/SearchViewModel.kt
+++ app/src/main/java/com/example/mediaapp/viewModels/SearchViewModel.kt
@@ -1,22 +1,25 @@
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.mediaapp.apirequests.APIHandler
 import com.example.mediaapp.models.TMDBMovie
 import com.example.mediaapp.repos.SearchRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class SearchViewModel(private val searchRepository: SearchRepository) : ViewModel() {
+class SearchViewModel() : ViewModel() {
+
+    private val apiHandler = APIHandler()
+    private val searchRepository = SearchRepository(apiHandler)
 
     private val _searchQuery = MutableStateFlow("")
-    val searchQuery: StateFlow<String> = _searchQuery
 
     private val _searchResults = MutableStateFlow<List<TMDBMovie>>(emptyList())
     val searchResults: StateFlow<List<TMDBMovie>> = _searchResults
 
     private val _isSearchActive = MutableStateFlow(false)
     val isSearchActive: StateFlow<Boolean> = _isSearchActive
-
 
     private val _error = MutableStateFlow<String?>(null)
     val error: StateFlow<String?> = _error
@@ -55,3 +58,4 @@ class SearchViewModel(private val searchRepository: SearchRepository) : ViewMode
         _error.value = null
     }
 }
+
