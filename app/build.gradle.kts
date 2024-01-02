7696b3bc3a725a897199c3c072e6d92be3443c4e
PH4NTOM <Simonmadsen99@gmail.com>
2024-01-02 21:25:57 +0100

    Added some tests for APIHandler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,11 +89,16 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.engage:engage-core:1.3.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    //Test Implementations
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation("io.mockk:mockk:1.13.8")
+
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
---
+++ app/src/test/java/com/example/mediaapp/APIHandlerUnitTest.kt
@@ -0,0 +1,63 @@
+package com.example.mediaapp
+
+import androidx.annotation.DisplayContext
+import org.junit.Test
+import org.junit.Assert.*
+import com.example.mediaapp.apirequests.APIHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+class APIHandlerUnitTest {
+    private val api = APIHandler()
+    @Test
+    //Testing that the API Call GetPopularMovies does not return an empty result
+    fun testGetPopularMoviesIsNotEmpty() {
+        runBlocking {
+            delay(1000)
+            val popularMovies = api.getPopularMovies("week")
+            if(popularMovies != null) {
+                assertFalse(popularMovies.results.isEmpty())
+                assertEquals(1, popularMovies.page)
+                assertEquals(1000, popularMovies.total_pages)
+                assertEquals(20000, popularMovies.total_results)
+            }
+        }
+    }
+
+    @Test
+    //Testing that we're getting the expected data from Search for Oppenheimer
+    fun testSearchForMovie() {
+        runBlocking {
+            delay(1000)
+            val searchResults = api.searchForMovie("Oppenheimer")
+            if(searchResults != null) {
+                assertEquals("Oppenheimer", searchResults.results[0].title)
+                assertEquals("2023-07-19", searchResults.results[0].release_date)
+                assertEquals("en", searchResults.results[0].original_language)
+                assertEquals(872585, searchResults.results[0].id)
+                assertEquals(1, searchResults.page)
+                assertEquals(2, searchResults.total_pages)
+                assertEquals(22, searchResults.total_results)
+            }
+        }
+    }
+
+    @Test
+    //Testing that we're getting the expected data from Oppenheimer
+    fun testGetMovieDetail() {
+        runBlocking {
+            delay(1000)
+            val movieDetail = api.getMovieDetail("872585")
+            if(movieDetail != null) {
+                assertEquals(false, movieDetail.adult)
+                assertEquals(100000000, movieDetail.budget)
+                assertEquals("Released", movieDetail.status)
+                assertEquals("Oppenheimer", movieDetail.title)
+                assertEquals("tt15398776", movieDetail.imdb_id)
+            }
+        }
+    }
+
+
+
+}
