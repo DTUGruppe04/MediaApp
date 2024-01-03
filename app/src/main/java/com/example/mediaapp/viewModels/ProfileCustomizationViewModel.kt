5ca8285afb865e1e786e4f9583dd99fd02c8ded1
Valdemar <s224289@dtu.dk>
2024-01-03 12:09:17 +0100

    moved all database handling to a seperat class, and made som changes to the code

---
+++ app/src/main/java/com/example/mediaapp/viewModels/ProfileCustomizationViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.mediaapp.viewModels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.mediaapp.models.DatabaseHandler
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class ProfileCustomizationViewModel : ViewModel() {
+    private val databaseHandler = DatabaseHandler()
+    val user = Firebase.auth.currentUser
+
+    var username = mutableStateOf("")
+    var name = mutableStateOf("")
+    var location = mutableStateOf("")
+    var description = mutableStateOf("")
+
+    fun updateAccountDetails() {
+        val userMap = hashMapOf(
+            "username" to username.value,
+            "name" to name.value,
+            "location" to location.value,
+            "description" to description.value
+        )
+        user?.let { databaseHandler.updateUserInDatabase(it.uid, userMap) }
+    }
+}
